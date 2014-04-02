2630915bd4fb71b3262fdac3743491a40a3ae496
spindelmanne <manne.fagerlind@smartbear.com>
2014-04-02 16:22:00 +0200

    SOAP-1993 XML names now created properly (used to fail for some corner cases)

--- soapui/src/main/java/com/eviware/soapui/support/StringUtils.java
+++ soapui/src/main/java/com/eviware/soapui/support/StringUtils.java
@@ -1,14 +1,19 @@
-/*
- *  SoapUI, copyright (C) 2004-2012 smartbear.com
+ /*
+ * Copyright 2004-2014 SmartBear Software
  *
- *  SoapUI is free software; you can redistribute it and/or modify it under the
- *  terms of version 2.1 of the GNU Lesser General Public License as published by 
- *  the Free Software Foundation.
+ * Licensed under the EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
  *
- *  SoapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *  See the GNU Lesser General Public License for more details at gnu.org.
- */
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the Licence for the specific language governing permissions and limitations
+ * under the Licence.
+*/
+
 
 package com.eviware.soapui.support;
 
@@ -315,11 +320,17 @@ public class StringUtils
 			}
 		}
 
-		str = result.toString();
-		if( str.length() > 0 && Character.isDigit(str.charAt(0)))
-			str = "_" + str;
+		String resultString  = result.toString();
+		return isValidXmlName( str ) ? resultString : "_" + resultString;
+	}
 
-		return str;
+	private static boolean isValidXmlName( String str )
+	{
+		if (str.isEmpty() || str.toLowerCase().startsWith( "xml" )) {
+			return false;
+		}
+		char firstCharacter = str.charAt( 0 );
+		return Character.isLetter( firstCharacter ) || firstCharacter == '_';
 	}
 
 	public static String[] merge( String[] incomingNames, String string )
